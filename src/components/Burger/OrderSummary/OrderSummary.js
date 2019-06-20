b3356c9f737b292875932ef33e66f4e8d136c541
Julius Ngwu <juliusngwu@Juliuss-MacBook-Pro.local>
2019-06-20 06:35:28 +0100

    5th commit

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import Auxx from '../../../hoc/Auxx';
+import React, { Component } from 'react';
+import Auxx from '../../../hoc/Auxx/Auxx';
 import Button from '../../UI/Button/Button';
  
-const OrderSummary = (props) => {
-    const ingredientSummaryList = Object.keys(props.ingredients)
-    .map(igKey => {
-        return (
-            <li key={igKey}>
-                <span style={{textTransform: "capitalize"}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li>);
+class OrderSummary extends Component{
+    componentWillUpdate () {
+        console.log('[Order Summary will update]');
+        
+    }
+    render() {
+        const ingredientSummaryList = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{textTransform: "capitalize"}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                    </li>);
     });
-    return (
-    <Auxx>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>
-            {ingredientSummaryList}
-        </ul>
-        <p><strong>Total Price: {props.price.toFixed(2) }</strong></p>
-        <p>Continue to Checkout?</p> 
-        <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-        <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Auxx>)
-}
+        return (
+            <Auxx>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummaryList}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2) }</strong></p>
+                <p>Continue to Checkout?</p> 
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Auxx>
+        );
+    }
+} 
 
 export default OrderSummary;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Auxx from '../../../hoc/Auxx';
+import Auxx from '../../../hoc/Auxx/Auxx';
 
 const SideDrawer = (props) => {
     let attachedClasses = [SideDrawer, "Close"];
